74527d8ad5c6fcd1be00a77d2b5ea2c611cf34be
Eric Nicdao <96414573+ericnicdao069@users.noreply.github.com>
2023-08-31 20:33:08 +0800

    Minor Fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,23 +8,21 @@
         <div class="row mb-2">
             <div class="col-sm-6">
                 <h1 class="m-0 text-dark">Dashboard</h1>
-            </div><!-- /.col -->
+            </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
                     <li class="breadcrumb-item active">Dashboard</li>
                 </ol>
-            </div><!-- /.col -->
-        </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
+            </div>
+        </div>
+    </div>
 </div>
-<!-- Main content -->
 <section class="content">
     <div class="container-fluid">
-    <!-- Small boxes (Stat box) -->
         <div class="row">
             <div class="col">
-                Fashboard
+                Dashboard
             </div>
         </div>
     </div>
