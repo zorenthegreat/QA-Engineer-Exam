b6d6725dc6641d9132958cc7e02fcf75d642e130
zorenthegreat <renrenaguilargates@gmail.com>
2024-06-20 13:59:30 +0800

    modified:   playwright/firstTest.spec.cjs

--- playwright/firstTest.spec.cjs
+++ playwright/firstTest.spec.cjs
@@ -13,16 +13,6 @@ test('Login Test' , async ({page}) => {
     await page.goto('http://localhost:8000/')
     await page.pause();
 
-    // await page.getByRole('link', { name: 'Create', exact: true }).click();
-    // await page.locator('input[type="text"]').click();
-    // await page.locator('input[type="text"]').fill('test');
-    // await page.getByRole('combobox').selectOption('2');
-    // await page.getByLabel('Rich Text Editor').getByRole('paragraph').click();
-    // await page.getByLabel('Editor editing area: main').fill('Test Description');
-    // await page.pause()
-    // await page.getByText('Next').click();
-    // await page.getByRole('link', { name: '+' }).setInputFiles('/home/zoren/downloads/logo.png');
-    // await page.getByText('Next').click();
-    // await page.pause()
+  
     
 })
